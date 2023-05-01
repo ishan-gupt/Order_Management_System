d5f6e9afea43d81737ef88e7966f114ccada2904
ishan-gupt <gupta.ishan@hotmail.com>
2023-05-01 14:20:43 +0530

    Import for user  added

--- routers/apiRoutes.js
+++ routers/apiRoutes.js
@@ -1,14 +1,19 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const{getAllOrders,placeOrder}=require("../controllers/orderController");
+const {
+  getAllOrders,
+  placeOrder,
+  userCheck,
+  userSignup,
+} = require("../controllers/orderController");
+const { adminSignup, adminLogin } = require("../controllers/adminController");
 
-router.post('/orders/', placeOrder);
-router.get('/orders/:storeid',getAllOrders);
-const {adminSignup , adminLogin}=require("../controllers/adminController")
+router.post("/orders/", placeOrder);
+router.get("/orders/:storeid", getAllOrders);
 
-router.post('/admin/signup', adminSignup);
-router.post('/admin/login',adminLogin);
+router.post("/admin/signup", adminSignup);
+router.post("/admin/login", adminLogin);
 router.get("/greet", (req, res) => {
-    res.send(`Hello, Ishan`);
-  });
-module.exports = router
+  res.send(`Hello, Ishan`);
+});
+module.exports = router;
