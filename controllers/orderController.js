c631db29b150786996daec430889091a60dc71e3
ishan-gupt <gupta.ishan@hotmail.com>
2023-05-01 14:20:18 +0530

    User check added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,40 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const orderSchema = require("../models/order");
-const userSchema=require("../models/user");
+const userSchema = require("../models/user");
 const User = mongoose.model("User", userSchema);
 const Order = mongoose.model("Order", orderSchema);
 
 const getAllOrders = async (req, res) => {
-    try {
-      const { storeid } = req.params;
-      const orders = await Order.find({ storeid }).populate("customer");
-      res.status(200).json(orders);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to retrieve orders" });
-    }
-  };
-const placeOrder = async (req, res) => {
-    try {
-      const { name, address, contact, storeid, order_details } = req.body;
-  
-      // Find or create user based on name and contact
-      let user = await User.findOne({ name, contact });
-      if (!user) {
-        user = new User({ name, address, contact });
-        await user.save();
-      }
-  
-      // Create order
-      const order = new Order({ customer: user._id, storeid, order_details });
-      await order.save();
-  
-      res.status(201).json(order);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to place order" });
-    }
-  };
-  module.exports={
-    getAllOrders:getAllOrders,
-    placeOrder:placeOrder,
-
+  try {
+    const { storeid } = req.params;
+    const orders = await Order.find({ storeid }).populate("customer");
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve orders" });
   }
+};
+const placeOrder = async (req, res) => {
+  try {
+    const { name, address, contact, storeid, order_details } = req.body;
+
+    // Find or create user based on name and contact
+    let user = await User.findOne({ name, contact });
+    if (!user) {
+      user = new User({ name, address, contact });
+      await user.save();
+    }
+
+    // Create order
+    const order = new Order({ customer: user._id, storeid, order_details });
+    await order.save();
+
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to place order" });
+  }
+};
+const userCheck = async (req, res) => {
+  try {
+    const { contact } = req.body;
+    const userExist = await User.findOne({ contact });
+    if (userExist) {
+      res.status(200).json(admin);
+    } else {
+      res.status(401).json({ error: "User does not exist" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to check for User" });
+  }
+};
+
+const userSignup = async (req, res) => {
+  try {
+    const { name, address, contact } = req.body;
+    const user = new User({ name, address, contact });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to signup user" });
+  }
+};
+
+module.exports = {
+  getAllOrders: getAllOrders,
+  placeOrder: placeOrder,
+  userCheck: userCheck,
+  userSignup: userSignup,
+};
