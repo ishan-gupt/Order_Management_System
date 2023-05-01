c8e8036d2959f99db0bd073595041bc605095f29
ishan-gupt <gupta.ishan@hotmail.com>
2023-05-01 12:25:18 +0530

    Updated Signup

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,35 +1,38 @@
-const mongoose = require('mongoose');
-const adminSchema= require("../models/admin");
+const mongoose = require("mongoose");
+const adminSchema = require("../models/admin");
 
 const Admin = mongoose.model("Admin", adminSchema);
 
-
-const adminSignup= async (req, res) => {
-    try {
-      const { name, password, storeid } = req.body;
+const adminSignup = async (req, res) => {
+  try {
+    const { name, password, storeid } = req.body;
+    const adminExist = await Admin.findOne({ name });
+    if (adminExist) {
+      res.status(401).json({ error: "Username Exists" });
+    } else {
       const admin = new Admin({ name, password, storeid });
       await admin.save();
       res.status(201).json(admin);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to signup admin" });
     }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to signup admin" });
   }
-  const adminLogin = async (req, res) => {
-    try {
-      const { name, password } = req.body;
-      const admin = await Admin.findOne({ name, password });
-      if (admin) {
-        res.status(200).json(admin);
-      } else {
-        res.status(401).json({ error: "Invalid credentials" });
-      }
-    } catch (error) {
-      res.status(500).json({ error: "Failed to login admin" });
+};
+const adminLogin = async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const admin = await Admin.findOne({ name, password });
+    if (admin) {
+      res.status(200).json(admin);
+    } else {
+      res.status(401).json({ error: "Invalid credentials" });
     }
-  };
-  
-  module.exports={
-    adminSignup:adminSignup,
-    adminLogin:adminLogin,
+  } catch (error) {
+    res.status(500).json({ error: "Failed to login admin" });
+  }
+};
 
-  }
+module.exports = {
+  adminSignup: adminSignup,
+  adminLogin: adminLogin,
+};
